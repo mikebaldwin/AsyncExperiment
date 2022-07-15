a607a6c1eb7861d7bee94a6e3ed2b44589d4d4f1
Mike Baldwin <mikembaldwin@gmail.com>
2022-07-15 06:40:35 -0400

    Add tempalte project

---
+++ AsyncExperiment/AsyncExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncExperimentApp.swift
+//  AsyncExperiment
+//
+//  Created by Mike Baldwin on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncExperiment/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  AsyncExperiment
+//
+//  Created by Mike Baldwin on 7/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
